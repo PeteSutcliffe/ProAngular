979ec634a79c292d26555d9afcd336aed3fd3628
Peter Sutcliffe <sutcliffe_peter@hotmail.com>
2014-09-23 11:11:18 +0100

    Completed administration

--- ProAngular/Controllers/ProductsController.cs
+++ ProAngular/Controllers/ProductsController.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Http;
 using ProAngular.Infrastructure;
@@ -14,6 +14,31 @@ namespace ProAngular.Controllers
         public IEnumerable<Product> Get()
         {
             return _db.Products.ToList();
+        }
+
+        public Product Get(int id)
+        {
+            return _db.Products.Find(id);
+        }
+
+        public void Post(int id, [FromBody]Product product)
+        {
+            _db.Products.Attach(product);
+            _db.Entry(product).State = EntityState.Modified;            
+            _db.SaveChanges();
+        }
+
+        public Product Post([FromBody]Product product)
+        {
+            _db.Products.Add(product);
+            _db.SaveChanges();
+            return product;
+        }
+
+        public void Delete(int id)
+        {
+            _db.Products.Remove(_db.Products.Find(id));
+            _db.SaveChanges();
         }
     }
 }
